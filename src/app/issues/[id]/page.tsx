3638c284f2eeade41d392ecd63a193a34d05b664
Julliane <dipaulajulliane@gmail.com>
2026-02-26 13:47:15 -0300

    :sparkles: feat: Formulário de Comentários

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,8 +1,14 @@
-import { ArchiveIcon, MoveLeftIcon, ThumbsUpIcon } from 'lucide-react'
+import {
+  ArchiveIcon,
+  MessageCirclePlusIcon,
+  MoveLeftIcon,
+  ThumbsUpIcon,
+} from 'lucide-react'
 import type { Metadata } from 'next'
 import Link from 'next/link'
 import { Suspense } from 'react'
 import { Button } from '@/components/button'
+import { Input } from '@/components/input'
 import { getIssue } from '@/http/get-issue'
 import { IssueCommentsList } from './issue-comments/issue-comments-list'
 import { IssueCommentsSkeleton } from './issue-comments/issue-comments-skeleton'
@@ -67,7 +73,19 @@ export default async function IssuePage({ params }: IssuePageProps) {
       <div className="flex flex-col gap-2">
         <span className="font-semibold">Comments</span>
 
-        <form />
+        <form className="relative w-full">
+          <Input
+            className="bg-navy-700 h-11 pr-24 w-full"
+            placeholder="Leave a comment..."
+          />
+          <button
+            type="submit"
+            className="flex items-center gap-2 text-indigo-400 absolute right-3 top-1/2 -translate-y-1/2 text-xs hover:text-indigo-300 cursor-pointer disabled:opacity-50"
+          >
+            Publish
+            <MessageCirclePlusIcon className="size-3" />
+          </button>
+        </form>
 
         <div className="mt-3">
           <Suspense fallback={<IssueCommentsSkeleton />}>
