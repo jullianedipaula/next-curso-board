9316d9264eb5a4085aa8c9810f66ceb6e661a0ba
Julliane <dipaulajulliane@gmail.com>
2026-02-25 22:29:46 -0300

    :lipstick: feat: Estrutura da Página de Issue

--- src/app/issues/header/index.tsx
+++ src/app/issues/header/index.tsx
@@ -1,5 +1,4 @@
 import { UserButton } from '@/components/header/user-button'
-import { SearchInput } from './search-input'
 
 export function Header() {
   return (
@@ -12,8 +11,6 @@ export function Header() {
       </div>
 
       <div className="flex items-center gap-4">
-        <SearchInput />
-
         <UserButton />
       </div>
     </header>
