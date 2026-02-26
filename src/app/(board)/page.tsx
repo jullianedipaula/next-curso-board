67f989fcc07cc5c6a822aa8eaa9cb08c6abc3e66
Julliane <dipaulajulliane@gmail.com>
2026-02-25 22:20:11 -0300

    :sparkles: feat: Página de Issue

--- src/app/(board)/page.tsx
+++ src/app/(board)/page.tsx
@@ -40,7 +40,7 @@ export default async function Board({ searchParams }: BoardProps) {
             </div>
           ) : (
             issues.backlog.map((issue) => (
-              <Card.Root key={issue.id}>
+              <Card.Root href={`/issues/${issue.id}`} key={issue.id}>
                 <Card.Header>
                   <Card.Number>ISS-{issue.issueNumber}</Card.Number>
                   <Card.Title>{issue.title}</Card.Title>
@@ -82,7 +82,7 @@ export default async function Board({ searchParams }: BoardProps) {
             </div>
           ) : (
             issues.todo.map((issue) => (
-              <Card.Root key={issue.id}>
+              <Card.Root href={`/issues/${issue.id}`} key={issue.id}>
                 <Card.Header>
                   <Card.Number>ISS-{issue.issueNumber}</Card.Number>
                   <Card.Title>{issue.title}</Card.Title>
@@ -124,7 +124,7 @@ export default async function Board({ searchParams }: BoardProps) {
             </div>
           ) : (
             issues.in_progress.map((issue) => (
-              <Card.Root key={issue.id}>
+              <Card.Root href={`/issues/${issue.id}`} key={issue.id}>
                 <Card.Header>
                   <Card.Number>ISS-{issue.issueNumber}</Card.Number>
                   <Card.Title>{issue.title}</Card.Title>
@@ -166,7 +166,7 @@ export default async function Board({ searchParams }: BoardProps) {
             </div>
           ) : (
             issues.done.map((issue) => (
-              <Card.Root key={issue.id}>
+              <Card.Root href={`/issues/${issue.id}`} key={issue.id}>
                 <Card.Header>
                   <Card.Number>ISS-{issue.issueNumber}</Card.Number>
                   <Card.Title>{issue.title}</Card.Title>
---
+++ src/app/issues/[id]/page.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next'
+import { getIssue } from '@/http/get-issue'
+
+interface IssuePageProps {
+  params: Promise<{ id: string }>
+}
+
+export const generateMetadata = async ({
+  params,
+}: IssuePageProps): Promise<Metadata> => {
+  const { id } = await params
+
+  const issue = await getIssue({ id })
+
+  return {
+    title: `Issue ${issue.title}`,
+  }
+}
+
+export default async function IssuePage({ params }: IssuePageProps) {
+  const { id } = await params
+
+  const issue = await getIssue({ id })
+
+  return <pre>{JSON.stringify(issue, null, 2)}</pre>
+}
